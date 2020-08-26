c45401a3411f8ebf8ba4902ed856fb20b2f6f4e3
jay <dyjay15@gmail.com>
2020-08-26 14:48:38 +0800

    install react router dom and explore the function of it

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 
 import Homepage from './pages/homepage/Homepage';
 
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+);
+
 function App() {
   return (
-    <div>
-      <Homepage />
-    </div>
+    <Switch>
+      <Route exact path='/' component={Homepage} />
+      <Route path='/shop/hats' component={HatsPage} />
+    </Switch>
   );
 }
 
